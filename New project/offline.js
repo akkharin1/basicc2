ae7b1a26fa82f81f87feb16dfa45c6337ffee127
akkharin1 <69181084+akkharin1@users.noreply.github.com>
2021-10-07 14:29:29 +0700

    s
    s

---
+++ New project/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1633590665,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/fruit-sheet0.png",
+		"images/playbutton-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/leftbutton-sheet0.png",
+		"images/rightbutton-sheet0.png",
+		"images/upbutton-sheet0.png",
+		"images/replybutton-sheet0.png",
+		"media/haha.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
